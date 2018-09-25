fe97e2305d2b434d87d2f1e20364879b8574882a
Wesley Sprague <wesleysprague@yahoo.com>
2018-09-25 10:47:06 -0700

    added script.js file, initialized variables

---
+++ 11 - Custom HTML5 Video Player/scripts.js
@@ -0,0 +1,7 @@
+const player = document.QuerySelector('.player');
+const video = player.QuerySelector('.viewer');
+const progress = player.QuerySelector('.progress');
+const progressBar = player.QuerySelector('.progress__filled');
+const toggle = player.QuerySelector('.toggle')
+const skipButtons = player.QuerySelectorAll('[data-skip]')
+const ranges = player.QuerySelectorAll('.player__slider')
