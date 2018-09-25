3e2cd395ab91579123566ed804fccb2dd0085db0
Wesley Sprague <wesleysprague@yahoo.com>
2018-09-25 16:20:39 -0700

    added range handle update method to adjust speed of video

--- 11 - Custom HTML5 Video Player/scripts.js
+++ 11 - Custom HTML5 Video Player/scripts.js
@@ -25,8 +25,17 @@ function skip(){
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate(){
+  video[this.name] = this.value
+}
+function handleProgress(){
+
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton)
 video.addEventListener('pause', updateButton)
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click',skip));
+ranges.forEach(range => range.addEventListener('change',handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove',handleRangeUpdate));
