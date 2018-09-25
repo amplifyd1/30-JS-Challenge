9e0d9af5e1e5611320101f13eb82486774df26a9
Wesley Sprague <wesleysprague@yahoo.com>
2018-09-25 11:02:08 -0700

    created toggle play function and added event listeners for play, pause, and toggle

--- 11 - Custom HTML5 Video Player/scripts.js
+++ 11 - Custom HTML5 Video Player/scripts.js
@@ -5,3 +5,14 @@ const progressBar = player.QuerySelector('.progress__filled');
 const toggle = player.QuerySelector('.toggle')
 const skipButtons = player.QuerySelectorAll('[data-skip]')
 const ranges = player.QuerySelectorAll('.player__slider')
+
+
+function togglePlay(){
+  if(video.paused){
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+video.addEventListener('click', tooglePlay);
+toogle.addEventListener('click', tooglePlay);
